24c13cc9feae1969aa359dee40fb63029be0c27f
winstonbarbe <winston.barbe@gmail.com>
2024-12-19 09:47:46 -0500

    Creates employee file with two employees

---
+++ employee.rb
@@ -0,0 +1,13 @@
+employee1 = {
+  first_name: "John",
+  last_name: "Smith",
+  salary: 70000
+}
+employee2 = {
+  first_name: "Jane",
+  last_name: "Smith",
+  salary: 80000
+}
+
+puts "#{employee1[:first_name]} #{employee1[:last_name]} makes $#{employee1[:salary]}"
+p "#{employee2[:first_name]} #{employee2[:last_name]} makes $#{employee2[:salary]}"
